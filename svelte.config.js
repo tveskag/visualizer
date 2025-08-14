af5374c0f40921d0044e9190bdb54b5187671efd
Asger Viggo <Asgervkn@gmail.com>
2025-08-14 18:55:50 +0200

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,9 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			 //base: process.argv.includes('dev') ? '' : '/visualizer'
+			 base: '/visualizer'
+			//base: '/page'
 		}
 	}
 };
